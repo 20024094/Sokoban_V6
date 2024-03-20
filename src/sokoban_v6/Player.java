394cfe7ed2b68dc1990ae8e12f9b1ce9035e41a5
danie <danie@Daniel.home>
2024-03-20 22:43:45 +0000

    OOP version 6 version with GUI builder.

---
+++ src/sokoban_v6/Player.java
@@ -0,0 +1,16 @@
+
+package sokoban_v6;
+
+/**
+ *
+ * @author danie
+ */
+
+public class Player extends MapElement {
+
+    public Player() {
+        setSymbol("P"); // 'P' can be a placeholder symbol for the player
+        setImgFileName("/graphics/WarehouseKeeper.png"); // Set the image file name for the player
+    }
+    
+}

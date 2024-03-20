4befecdaae6f82918bc4b3181d0cd180dad8193f
danie <danie@Daniel.home>
2024-03-20 22:43:06 +0000

    OOP version 6 version with GUI builder.

---
+++ src/sokoban_v6/Floor.java
@@ -0,0 +1,13 @@
+package sokoban_v6;
+
+/**
+ *
+ * @author danie
+ */
+public class Floor extends MapElement {
+    
+    Floor() {
+        setSymbol(".");
+        setImgFileName("/graphics/Floor.png");
+    }
+}
